a713e8b14575b876b93fb8a1bc619591632cacd8
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-08-12 20:54:22 +0300

    Added chat with firebase connectivity

--- Firebase.js
+++ Firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import Constants from 'expo-constants'
 
 // Your web app's Firebase configuration
@@ -15,5 +16,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db= getFirestore(app);
 
 export { auth };
+export { db };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,45 @@
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { GiftedChat } from 'react-native-gifted-chat'
-import React, { useState } from 'react'
+import React, { useLayoutEffect, useState, useCallback } from 'react'
 import { signOut } from 'firebase/auth'
-import { auth } from '../Firebase'
+import { auth, db } from '../Firebase'
+import { collection, addDoc, orderBy, query, onSnapshot, getDocs } from "firebase/firestore";
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const HomeScreen = ({ navigation }) => {
   const [messages, setMessages] = useState([])
+  useLayoutEffect(() => {
+    const getMessages = async () => {
+      const q = await getDocs(collection(db, "chats"), orderBy('createdAt'));
+
+      setMessages(q.docs.map(item => ({
+        _id: item.data()._id,
+        createdAt: item.data().createdAt.toDate(),
+        text: item.data().text,
+        user: item.data().user
+      })))
+      console.log(messages)
+    }
+    return () => getMessages()
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+
+    const {_id, createdAt, text, user} = messages[0]
+    const addMessageToDB = async () => {
+      await addDoc(collection(db, 'chats'), {
+        _id,
+        createdAt,
+        text,
+        user
+      })
+    }
+
+    return addMessageToDB()
+  }, [])
 
   const handleSignOut = () => {
     signOut(auth)
@@ -22,9 +56,16 @@ const HomeScreen = ({ navigation }) => {
           <Text style={styles.buttonText}>Sign out</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.chat}>
-
-      </View>
+      <SafeAreaView style={{ flex: 1, width: '100%' }}>
+        <GiftedChat
+          messages={messages}
+          user={{
+            _id: auth?.currentUser?.email,
+            avatar: 'https://i.pravatar.cc/300'
+          }}
+          messageContainerStyle={{ backgroundColor: 'white' }}
+          onSend={onSend} />
+      </SafeAreaView>
     </View>
   )
 }
@@ -33,7 +74,8 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center'
+    alignItems: 'center',
+    flex: 1
   },
   buttonContainer: {
     backgroundColor: 'white',
